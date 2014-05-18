bfd5280a4c3b11377460553a10ccfcdfc1ae8c2d
Arvind Mehra <your.bloomberg490@gmail.com>
2014-05-18 22:28:10 +0530

    heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marble::Application.config.secret_key_base = '2761737bce767f01221fd7e59b056f390fed220743a17fb6b160da129794d34423ceaa8a9c2123047e6b45a9919caa609287f43113b81539836641252b7c2ad4'
