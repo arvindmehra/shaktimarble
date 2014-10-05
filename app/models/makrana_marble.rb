f247349dc40032ec762f26937d983b5d915cf0dc
arvind <bloomberg490@gmail.com>
2014-10-05 17:19:09 +0530

    devise with side menu

--- app/models/makrana_marble.rb
+++ app/models/makrana_marble.rb
@@ -1,6 +1,6 @@
 class MakranaMarble < ActiveRecord::Base
   mount_uploader :makrana_marble_pics, MakranaMarblePicsUploader
-  validates_presence_of :name,:makrana_marble_pics
+  validates_presence_of :name
 
   CATEGORY = ["albeta","dungri","kumari","makrana_kumari","matevari_kumari","white_marble"]
 
