32dffe93e8a0198e3ae71824329e1873ae2aa582
arvind <bloomberg490@gmail.com>
2014-11-09 20:22:33 +0530

    awesome-home-page

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -1,8 +1,6 @@
-//= require jquery
-//= require jquery.min
-//= require jquery.lettering
-//= require bootstrap
-
-$(function() {
-  $("#letter-container h2 a").lettering();
+jQuery(document).ready(function($){
+  //open-close submenu on mobile
+  $('.cd-main-nav').on('click', function(event){
+    if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
+  });
 });
